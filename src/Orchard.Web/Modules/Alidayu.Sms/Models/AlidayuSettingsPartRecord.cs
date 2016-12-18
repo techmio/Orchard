f661942018ad7d28edd79b366622f724809327d4
TECHMIO-PC\techmio <techmio@qq.com>
2016-12-18 17:15:09 +0800

    alidayu setttings part recoards

---
+++ src/Orchard.Web/Modules/Alidayu.Sms/Models/AlidayuSettingsPartRecord.cs
@@ -0,0 +1,22 @@
+﻿using Orchard.ContentManagement.Records;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alidayu.Sms.Models
+{
+
+    public class AlidayuSettingsPartRecord : ContentPartRecord
+    {
+        public virtual string SmsFreeSignName { get; set; }
+
+
+        public virtual string SmsParam { get; set; }
+
+
+        public virtual string RecNum { get; set; }
+        public virtual string SmsTemplateCode { get; set; }
+
+    }
+}
